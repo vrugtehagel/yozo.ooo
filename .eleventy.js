ca2b8705cf04aeeaaf4bb21f2009efb5f067f036
vrugtehagel <vrugtehagel@pm.me>
2024-04-10 18:26:20 +0200

    Add passthrough for more stuff and add versions as global data

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,22 @@
+import * as fs from 'node:fs/promises'
+
+
 export default function(config){
 	config.setLayoutResolution(false)
 	config.setLiquidOptions({ extname: '' })
-	config.addPassthroughCopy('src/**/*.{css,js,yz,svg,ttf,png}')
 
+	config.addGlobalData('versions', async () => {
+		const json = await fs.readFile('yozo/versions.json', 'utf8')
+		return JSON.parse(json)
+	})
+
+	config.addPassthroughCopy('src/**/*.{css,js,yz,svg,ttf,png,ico,txt,json}')
 	config.addPassthroughCopy({
 		'yozo/versions.json': 'versions.json',
 		'yozo/archive': 'archive',
 		'yozo/tests': '-/tests',
 	})
+
 
 	return { dir: { input: 'src', output: 'dist' } }
 }

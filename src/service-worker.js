cadbd9163648db588dc9d0cc1b66dd82dcc6f2f1
vrugtehagel <vrugtehagel@pm.me>
2024-05-31 14:43:45 +0200

    Revert "Switch service worker to module"
    This reverts commit 6d40137d00d2edfbcd546f9fa87d079436733352.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,10 @@
-import '/archive/lib-0.5.0.js'
-import '/-/js/service-worker/service-worker.js'
-import '/-/js/web-server/service-worker.js'
+// Modules are not fully supported yet, so there are modified versions
+// for each module that simply throw things into the global scope.
+// That way, we can use importScripts and still separate related code.
+
+// Refactor when ES6 modules are supported in service workers.
+
+importScripts('/archive/lib-0.3.8.js')
+importScripts('/-/js/service-worker/service-worker.js')
+importScripts('/-/js/context-messenger/service-worker.js')
+importScripts('/-/js/web-server/service-worker.js')

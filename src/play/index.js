cded7b44aa2b53597f5444e7eb6632a8d2f2219b
vrugtehagel <vrugtehagel@pm.me>
2024-06-09 10:14:09 +0200

    Preload some components in playground

---
+++ src/play/index.js
@@ -0,0 +1,9 @@
+window.yozo.register('/-/yz/play/play-file-editor.yz')
+window.yozo.register('/-/yz/play/play-preview.yz')
+window.yozo.register('/-/yz/play/play-session.yz')
+window.yozo.register('/-/yz/play/play-space.yz')
+window.yozo.register('/-/yz/play/play-toolbar.yz')
+
+window.yozo.register('/-/yz/ui/ui-icon.yz')
+window.yozo.register('/-/yz/ui/ui-button.yz')
+window.yozo.register('/-/yz/ui/ui-dropdown.yz')

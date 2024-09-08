2380fab7fdf65a699210d9b6f3105b324b7582f4
vrugtehagel <vrugtehagel@pm.me>
2024-09-08 11:37:26 +0200

    Minor package maintenance

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 import { EleventyRenderPlugin } from '@11ty/eleventy'
-import { customMarkdown } from './plugins/custom-markdown.js'
 import EleventyDocumentOutline from '@vrugtehagel/eleventy-document-outline'
 import EleventyAssetHash from '@vrugtehagel/eleventy-asset-hash'
+import { customMarkdown } from './plugins/custom-markdown.js'
 
 export const config = {
 	dir: {input: 'src', output: 'dist'},
@@ -14,14 +14,12 @@ export default function(config){
 	config.setLiquidOptions({extname: ''})
 	config.addPlugin(customMarkdown)
 	config.addPlugin(EleventyRenderPlugin)
-	config.addPlugin(EleventyDocumentOutline, {
-		headers: 'h1, h2, h3'
-	})
+	config.addPlugin(EleventyDocumentOutline, {headers: 'h1, h2, h3'})
 	config.addPlugin(EleventyAssetHash, {
 		maxLength: 10,
 		include: ['**/*.{html,css,js,yz}'],
 		exclude: ['/archive/*', '/lib-latest.js', '/dev-latest.js'],
-		includeAssets: ['**/*.{css,js}'],
+		includeAssets: ['**/*.{css,js,svg}'],
 		excludeAssets: ['/archive/*', '/lib-latest.js', '/dev-latest.js'],
 	})
 

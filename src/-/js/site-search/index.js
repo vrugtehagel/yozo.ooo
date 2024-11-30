1eb87e11a05048bb9eaf286a2d036f325de778da
vrugtehagel <vrugtehagel@pm.me>
2024-11-30 16:22:19 +0100

    Moving parts around for search and 404 page

--- src/-/js/site-search/index.js
+++ src/-/js/site-search/index.js
@@ -5,6 +5,19 @@ const url = new URL('./worker.js', import.meta.url)
 const worker = new Worker(url, {type: 'module'})
 const messenger = new ContextMessenger(worker, worker)
 
+function getIcon(url){
+	if(url.startsWith('/docs/')) return 'book'
+	if(url.startsWith('/blog/')) return 'chat'
+	if(url.startsWith('/tour/')) return 'learn'
+	if(url.startsWith('/play/')) return 'controller'
+	if(url.startsWith('/download/')) return 'download'
+	return null
+}
+
 export async function search(query){
-	return await messenger.send('search', {query})
+	const results = await messenger.send('search', {query})
+	return results.map(({title, url, score}) => {
+		const icon = getIcon(url)
+		return {title, url, score, icon}
+	})
 }
--- src/404/index.js
+++ src/404/index.js
@@ -4,25 +4,12 @@ const path = window.location.href.replace(window.origin, '')
 const decoded = decodeURIComponent(path)
 const query = decoded.replaceAll(/\W+/g, ' ')
 
-const results = (await search(query))
-	.slice(0, 3)
-
-function getIcon(url){
-	if(url.startsWith('/docs/')) return 'book'
-	if(url.startsWith('/blog/')) return 'chat'
-	if(url.startsWith('/tour/')) return 'learn'
-	if(url.startsWith('/play/')) return 'controller'
-	if(url.startsWith('/download/')) return 'download'
-}
-
-// Fix the layout shift
-await yozo.timeout(300)
+const results = (await search(query)).slice(0, 3)
 
 const wrapper = document.querySelector('#transition-wrapper')
 const ul = document.querySelector('#guess ul')
-for(const result of results){
-	const urlText = result.url.split('/').slice(1, -1).join(' › ')
-	const icon = getIcon(result.url)
+for(const {url, title, icon} of results){
+	const urlText = url.split('/').slice(1, -1).join(' › ')
 	const li = document.createElement('li')
 	li.innerHTML = `
 		<a>
@@ -32,8 +19,8 @@ for(const result of results){
 			<span class=url></span>
 		</a>
 	`
-	li.querySelector('a').href = result.url
-	li.querySelector('.title').append(result.title)
+	li.querySelector('a').href = url
+	li.querySelector('.title').append(title)
 	li.querySelector('.url').append(urlText)
 	ul.append(li)
 }
@@ -43,6 +30,7 @@ if(results.length > 0){
 	const wrapper = document.querySelector('#transition-wrapper')
 	const guess = document.querySelector('#guess')
 
+	await yozo.timeout(300)
 	wrapper.style.height = '0px'
 	wrapper.hidden = false
 	await paint()
